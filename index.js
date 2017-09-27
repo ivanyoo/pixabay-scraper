755d8907d958bd33e572e9a5924a1900cc03731f
Paul Ivan Laureano <114344211@umail.ucc.ie>
2017-09-27 14:22:00 +0100

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,50 @@
 const request = require('request');
-const API_KEY =  '6523804-fb3da2d38e12b9dc997710e32';
-const URL =  `https://pixabay.com/api/?key=${API_KEY}`;
-
-request(URL, (err, response, body) => {
-   const hits = JSON.parse(body).hits;
-   hits.forEach(hit => {
-      console.log(`Webformat URL: ${hit.webformatURL}`);
-      console.log(`Tags: ${hit.tags}`)
-   });
+const async = require('async');
+const mysql = require('mysql');
+const config = require('./config.json');
+const perPage = 100;
+const total = 10000;
+let page = 1;
+async.whilst(() => {
+  // run for total amount of images
+  return page * perPage <= total;
+}, (callback) => {
+  // api call
+  let URL =  `https://pixabay.com/api/?key=${config.API_KEY}&page=${page}&per_page=${perPage}`;
+  request(URL, (err, response, body) => {
+    let values = [];
+    async.series([
+      (next) => {
+        const hits = JSON.parse(body).hits;
+        async.each(hits, (hit, nextHit) => {
+          // for each image put their webformatURL and tags(keywords) in array
+          values.push(`("${hit.webformatURL}","${hit.tags}")`);
+          nextHit();
+        }, next);
+      },
+      (next) => {
+        // store in database
+        const connection = mysql.createConnection(config.AWS_DB);
+        connection.connect();
+        connection.query(`INSERT INTO pixabay_images (webformatURL, tags) VALUES ${values.toString()}`, (err, results) => {
+          connection.end();
+          if (err) {
+            console.log(err);
+            return next(err);
+          }
+          next();
+        });
+      }
+    ], (err) => {
+      if (err) {
+        return callback(err);
+      }
+      page++;
+      console.log(`Done ${(page * perPage) - 100}`);
+      callback();
+    });
+  });
+}, (err) => {
+  console.log(err);
+  console.log('done');
 });
